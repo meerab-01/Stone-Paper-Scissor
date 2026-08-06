56312e6291cff62b31696e57c23080de970e9f61
Meerab <shahzadimeerab656@gmail.com>
2026-08-06 13:38:07 -0700

    hello

---
+++ app.js
@@ -0,0 +1,32 @@
+let userScore = 0;
+let compScore = 0;
+
+function playGame(userChoice) {
+
+    let choices = ['stone', 'paper', 'scissors'];
+    let computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    let result = "";
+
+    if (userChoice === computerChoice) {
+        result = "It's a Draw ";
+    }
+    else if (
+        (userChoice === "stone" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "stone") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "You Win ";
+        userScore++;
+    }
+    else {
+        result = "You Lose";
+        compScore++;
+    }
+
+    document.getElementById("result").innerText =
+        `You: ${userChoice} | Computer: ${computerChoice} → ${result}`;
+
+    document.getElementById("user-score").innerText = userScore;
+    document.getElementById("comp-score").innerText = compScore;
+}
